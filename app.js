ead3ee932ab2019df0ed54554b481290b2a9ee25
Patrick Dwyer <patrickjohanndwyer@gmail.com>
2022-06-08 17:08:09 -0600

    Forgot to update all long descriptions of projects. Updated with this log.

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.listen(port, function() {
         imgFileAlt: "View of Keeper App logo and note input bar.",
         title: "Keeper App",
         shortDescription: "An application to keep all your notes in.",
-        longDescription: "This is going to be the long description that will talk about the languagues, frameworks and methods used.",
+        longDescription: "This frontend was built using React. Some important concepts utilized in this project were declarative programming, state management and Javascript concepts (such as mapping, filtering, ternary operator, Logical AND, and arrow functions). What was interesting with this project was the ability to have different components interact with each other through properties and the ability to create and remove notes. The result is a modern looking user interface with built in functionality.",
         projectURL: "https://l2c5jb.csb.app/"
     },
 
@@ -49,7 +49,7 @@ app.listen(port, function() {
         imgFileAlt: "View of Keeper App logo and note input bar.",
         title: "ToDo List App",
         shortDescription: "An application to make your to do list on.",
-        longDescription: "This is going to be the long description that will talk about the languagues, frameworks and methods used.",
+        longDescription: "This frontend was built using React. Similar to the Keeper App hooks, props and the utilization of Javascript were key to functionality. This was a precursor to the Keeper App to help practice the concepts required. You can add notes by typing and clicking the Add button and delete by clicking on the list item. The data submitted is not persisted and resets upon refresh or navigation away from the page.",
         projectURL: "https://0lozsd.csb.app/"
     },
 
@@ -76,7 +76,7 @@ app.listen(port, function() {
         imgFileAlt: "Simon game tiles.",
         title: "The Simon Game",
         shortDescription: "Test your memory with this recall game.",
-        longDescription: "This is going to be the long description that will talk about the languagues, frameworks and methods used.",
+        longDescription: "One of my favorite projects I’ve worked on. This was setup from scratch where I went step by step. The first step was to create all the HTML elements required, then style them with CSS. Once this was completed, Javascript was used to bring functionality to this project. This project was written using JQuery. Some of the important concepts used were event listeners, logic statements, using DOM manipulation techniques for CSS animation and switch statements. Something I had added once the game was complete was a mobile friendly button to allow this game to be started without a key press but by clicking the button.",
         projectURL: "https://patrick-dwyer.github.io/TheSimonGame/"
     },
 
@@ -85,7 +85,7 @@ app.listen(port, function() {
         imgFileAlt: "Drum kit logo.",
         title: "Drum 🥁 Kit",
         shortDescription: "Play around with this online drum kit.",
-        longDescription: "This is going to be the long description that will talk about the languagues, frameworks and methods used.",
+        longDescription: "This game was a prerequisite to The Simon Game. Concepts that were learned and practiced during this project were loops, event listeners, switch statements, and nested functions.",
         projectURL: "https://patrick-dwyer.github.io/DrumKit-Site/"
 
     },
@@ -93,9 +93,9 @@ app.listen(port, function() {
     {
         imgFile: "/images/diceGame1v1Img.png",
         imgFileAlt: "Two dice with title of winner.",
-        title: "Dice Game 1 v 1",
+        title: "Random Dice 1 v 1",
         shortDescription: "Play with a friend on a luck based dice roll.",
-        longDescription: "This is going to be the long description that will talk about the languagues, frameworks and methods used.",
+        longDescription: "A fun game of chance between two dice. This was my first javascript project and involved logic statements, DOM techniques for altering elements, functions and loops. This was a pretty cool project because it let me see that some simple code can help build a simple game which felt quite rewarding.",
         projectURL: "https://patrick-dwyer.github.io/RandomDice-1v1/"
     },
 
@@ -107,9 +107,5 @@ app.listen(port, function() {
         longDescription: "This project was focused on honing skills in CSS and Bootstrap to make a good looking home page. Some of the key Bootstrap methods used are containers, cards, carousel, and navbar alongside custom CSS.",
         projectURL: "https://patrick-dwyer.github.io/Boostrap-Project-TInDog/"
     }
-
-    
-
-    
 
    ]
